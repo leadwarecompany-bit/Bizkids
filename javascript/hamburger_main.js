8ec071e84208337ac24de303f84696ad472a18fe
Myce-080 <philipezemba080@gmail.com>
2025-10-15 13:32:36 +0100

    wetsrd

--- javascript/hamburger_main.js
+++ javascript/hamburger_main.js
@@ -1,5 +1,6 @@
 const toggles = document.querySelectorAll(".togglers");
 const hamburgers = document.querySelectorAll(".main-hamburger-menu");
+const main_hamburgerIcon = document.querySelectorAll(".main-hamburger-icon")
 
 function mainnavtoggle() {
     toggles.forEach((toggle) => {
@@ -10,5 +11,8 @@ function mainnavtoggle() {
 hamburgers.forEach((hamburger) =>{
     hamburger.addEventListener("click", () => {
         mainnavtoggle()
+        main_hamburgerIcon.forEach((main_icon, i) => {
+            main_icon.classList.toggle("main-hamburger-icon-active")
+        });
     });
 });
